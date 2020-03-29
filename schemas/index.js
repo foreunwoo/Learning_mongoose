fe54a256072c40c6f67835bbe170b5f2059df852
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-03-29 19:09:55 +0900

    model modify

--- schemas/index.js
+++ schemas/index.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
     const connect = () => {
- mongoose.connect('mongodb://@localhost:27017/admin', {
-     dbName: 'nodejs',
+      if (process.env.NODE_ENV != 'production') {
+        mongoose.set('debug', true);
+      }
+      mongoose.connect('mongodb://root:admin123@localhost:27017/admin', {
+      dbName: 'nodejs',
  }, (error) => {
      if (error) {
          console.log('몽고디비 연결 에러', error);
@@ -16,7 +19,7 @@ connect();
  mongoose.connection.on('error', (error) => {
      console.error('몽고디비 연결 에러', error);
  });
- mongoose,connection.on('disconnected', (error) => {
+ mongoose.connection.on('disconnected', (error) => {
      console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.', error);
      connect();
  });
